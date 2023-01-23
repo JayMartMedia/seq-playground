f29a22d098ea52b53107fdd6dd2ed666a2eb3fa1
JayMartMedia <Jay@JayMartMedia.com>
2023-01-22 21:32:36 -0500

    feat: add dotnet seq implementation (not complete)

---
+++ dotnet-seq/Program.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+
+namespace dotnet_seq;
+
+// seq dotnet docs: https://docs.datalust.co/docs/logging-from-net
+// nlog specific docs: https://docs.datalust.co/docs/using-nlog
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        LogManager.LoadConfiguration("./NLog.config");
+
+        // var config = new NLog.Config.LoggingConfiguration();
+        // var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+        // config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+        // var logseq = new NLog.Targets.Seq({})
+        // LogManager.Configuration = config;
+
+        Logger logger = LogManager.GetCurrentClassLogger();
+        logger.Info("here is a message");
+        LogManager.Shutdown();
+    }
+}
