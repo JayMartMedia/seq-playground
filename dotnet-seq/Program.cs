27cd756650fb5c24306b54601f5e2747924279fe
JayMartMedia <Jay@JayMartMedia.com>
2023-01-29 22:06:49 -0500

    feat: add programmatic configuration of seq to dotnet project

--- dotnet-seq/Program.cs
+++ dotnet-seq/Program.cs
@@ -1,4 +1,7 @@
 ﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Targets.Seq;
 
 namespace dotnet_seq;
 
@@ -11,16 +14,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        LogManager.LoadConfiguration("./NLog.config");
+        // LogManager.LoadConfiguration("NLog.config");
 
-        // var config = new NLog.Config.LoggingConfiguration();
-        // var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
-        // config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
-        // var logseq = new NLog.Targets.Seq({})
-        // LogManager.Configuration = config;
+        // Uncomment below line to configure NLog programmatically
+        // ConfigureNlogProgrammatically();
 
         Logger logger = LogManager.GetCurrentClassLogger();
         logger.Info("here is a message");
         LogManager.Shutdown();
+    }
+
+    private static void ConfigureNlogProgrammatically() {
+        // create configuration
+        LoggingConfiguration config = new LoggingConfiguration();
+
+        // create targets
+        ConsoleTarget consoleTarget = new ConsoleTarget("logconsole");
+        SeqTarget seqTarget = new SeqTarget
+        {
+            ServerUrl = "http://localhost:5341",
+            ApiKey = "wy9A95L3ww0R0BTioZnQ"
+        };
+
+        // add rules to send to targets
+        config.AddRule(LogLevel.Info, LogLevel.Fatal, consoleTarget);
+        config.AddRule(LogLevel.Info, LogLevel.Fatal, seqTarget);
+
+        // set configuration
+        LogManager.Configuration = config;
     }
 }
